980c3141e6ef15f7319abefcead5af92e15f370a
ggomagundan <ggogun@gmail.com>
2013-10-19 04:25:40 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WaterPlant::Application.config.secret_key_base = '84624c2716ab7b1bf8251cd970625bfd4b3c12b817a9137c6add2e49499c235052ebcc5ea887c77cf5d1acba66113a08814592d5c119cf8a900502e5bcd75d0c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WaterPlant::Application.config.session_store :cookie_store, key: '_waterPlant_session'
